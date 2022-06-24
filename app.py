11b7c94c4f813ed97ea393dd6f1f2a1fde2a8d00
xulaing <khauv@live.fr>
2022-06-24 02:49:17 +0200

    final

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from model import recommenders, utils
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def quiz():
     # choose sample to show for quiz
     most_popular = recommenders.sample_popular()
@@ -14,74 +15,71 @@ def quiz():
     if request.method == 'POST':
         quiz_results = request.form.getlist("my_checkbox")
 
-        sample = random.sample(quiz_results,2) # for two categories
-        title = sample[0]
+        sample = random.sample(quiz_results, 2)  # for two categories
+        title = sample[0]  # title of one of the recipes liked
 
-        print(f'quiz results: {quiz_results}')
-        print(f'title: {title}')
+        ### LES PERSONNES AVEC DES GOÛTS SIMILAIRES ONT AUSSI AIMÉ ! ###
+        user_recommended = recommenders.quiz_user_user_recommender(
+            utils.create_new_user(quiz_results))
 
-        ### People with Similar Tastes Also Liked ###
-        user_recommended = recommenders.quiz_user_user_recommender(utils.create_new_user(quiz_results))
-        print(f'user_recommended: {user_recommended}')
+        ### PARCE QUE VOUS AVEZ AIMÉ XXXX ###
+        item_recommended = recommenders.item_item_recommender(
+            title=title, new_user=utils.create_new_user(quiz_results))
 
-        ### Because you liked X ###
-        item_recommended = recommenders.item_item_recommender(title=title, new_user=utils.create_new_user(quiz_results))
+        ### 2 CATEGORIES ###
         # from quiz results, get category, and randomly select 2 to return recipes in that category
-        print(f'item_recommended: {item_recommended}')
-        ### categories ###
         cat1 = utils.get_category(sample[0])
-        cat1_recommended = [utils.recipe_id_to_title(recipe) for recipe in utils.similar_to_cat(cat1)]
+        cat1_recommended = [utils.recipe_id_to_title(
+            recipe) for recipe in utils.similar_to_cat(cat1)]
 
         cat2 = utils.get_category(sample[1])
-        cat2_recommended = [utils.recipe_id_to_title(recipe) for recipe in utils.similar_to_cat(cat2)]
+        cat2_recommended = [utils.recipe_id_to_title(
+            recipe) for recipe in utils.similar_to_cat(cat2)]
 
-        cats_recommended = list([cat1_recommended,cat2_recommended])
-        print(f'cats_recommended: {cats_recommended}')
+        cats_recommended = list([cat1_recommended, cat2_recommended])
 
-        ### tastebreaker ###
-        tastebreaker = recommenders.item_item_recommender(title=title, new_user=utils.create_new_user(quiz_results), opposite=True)
-        print(f'tastebreaker: {tastebreaker}')
+        ### A DÉCOUVRIR ###
+        tastebreaker = recommenders.item_item_recommender(
+            title=title, new_user=utils.create_new_user(quiz_results), opposite=True)
 
-        # svd_recommended = recommenders.svd_recommender(8888888, new_user=utils.create_new_user(quiz_results))
-        # print(f'svd_recommended: {svd_recommended}')
+        ### LE TOP DU TOP ###
         all = user_recommended + item_recommended
-        # + svd_recommended
-
-        all = set(all) # remove duplicates
+        all = set(all)  # remove duplicates
         # remove recipes user has tried & sample 6
-        hybrid_recommended = random.sample([x for x in all if x not in utils.known_positives(8888888,new_user=utils.create_new_user(quiz_results))],6)
-
-        print(f'hybrid_recommended: {hybrid_recommended}')
+        hybrid_recommended = random.sample([x for x in all if x not in utils.known_positives(
+            5555555, new_user=utils.create_new_user(quiz_results))], 6)
 
         return render_template("result.html",
-        title = title,
+                               title=title,
 
-        cats = (list([cat1,cat2]), cats_recommended, [utils.get_url(utils.title_to_id(recipe)) for recipe in cats_recommended[0]], [utils.get_url(utils.title_to_id(recipe)) for recipe in cats_recommended[1]]),
-        # tuple, second element is the image url
-        most_popular=([utils.strip_filler(recipe) for recipe in most_popular],
-        [utils.get_url(utils.title_to_id(recipe)) for recipe in most_popular]),
+                               cats=(list([cat1, cat2]), cats_recommended, [utils.title_to_id(recipe) for recipe in cats_recommended[0]], [
+                                     utils.title_to_id(recipe) for recipe in cats_recommended[1]]),
+                               # tuple, second element is the image url
+                               most_popular=([utils.strip_filler(recipe) for recipe in most_popular],
+                                             [utils.title_to_id(recipe) for recipe in most_popular]),
 
-        quiz_results=([utils.strip_filler(recipe) for recipe in quiz_results],
-        [utils.get_url(utils.title_to_id(recipe)) for recipe in quiz_results]),
+                               quiz_results=([utils.strip_filler(recipe) for recipe in quiz_results],
+                                             [utils.title_to_id(recipe) for recipe in quiz_results]),
 
-        user_recommended=([utils.strip_filler(recipe) for recipe in user_recommended],
-        [utils.get_url(utils.title_to_id(recipe)) for recipe in user_recommended]),
+                               user_recommended=([utils.strip_filler(recipe) for recipe in user_recommended],
+                                                 [utils.title_to_id(recipe) for recipe in user_recommended]),
 
-        item_recommended=([utils.strip_filler(recipe) for recipe in item_recommended],
-        [utils.get_url(utils.title_to_id(recipe)) for recipe in item_recommended],utils.strip_filler(title))
-        ,
+                               item_recommended=([utils.strip_filler(recipe) for recipe in item_recommended],
+                                                 [utils.title_to_id(recipe) for recipe in item_recommended], utils.strip_filler(title)),
 
-        tastebreaker=([utils.strip_filler(recipe) for recipe in tastebreaker],
-        [utils.get_url(utils.title_to_id(recipe)) for recipe in tastebreaker],utils.strip_filler(title)),
+                               tastebreaker=([utils.strip_filler(recipe) for recipe in tastebreaker],
+                                             [utils.title_to_id(recipe) for recipe in tastebreaker], utils.strip_filler(title)),
 
-        hybrid_recommended = ([utils.strip_filler(recipe) for recipe in hybrid_recommended],
-        [utils.get_url(utils.title_to_id(recipe)) for recipe in hybrid_recommended])
-        )
+                               hybrid_recommended=([utils.strip_filler(recipe) for recipe in hybrid_recommended],
+                                                   [utils.title_to_id(recipe) for recipe in hybrid_recommended])
+                               )
 
     # landing screen
     return render_template("quiz.html",
-    most_popular=(most_popular,[utils.get_url(utils.title_to_id(recipe)) for recipe in most_popular])
-    )
+                           most_popular=(most_popular, [
+                               utils.title_to_id(recipe) for recipe in most_popular])
+                           )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -25,11 +25,13 @@ all_users = pd.read_csv("./data/users/all_users.csv")
 all_users.drop_duplicates(inplace=True)
 all_recipes = pd.read_csv("./data/recipes/all_recipes.csv")
 all_recipes.drop_duplicates(inplace=True)
-photo_urls = pd.read_csv("./data/photo_url/photo_urls.csv")
-photo_urls.drop_duplicates(inplace=True)
 recipe_lookup = all_recipes[["recipe_id","title"]]
 
-
+# Collaborative filtering for those with at least 3 reviews
+# ratings_by_user = all_users.groupby(["user_id","username"])[["rating"]].count().sort_values("rating",ascending=False)
+# at_least_3_ids = list(ratings_by_user[ratings_by_user["rating"]>=3].reset_index().user_id)
+# users3 = all_users[all_users.user_id.isin(at_least_3_ids)][["user_id","recipe_id","rating"]]
+# pickle.dump(users3, open("users3.pkl", "wb"))
 users3 = pickle.load(open("./pickle/users3.pkl","rb"))
 
 class utils:
@@ -39,6 +41,8 @@ class utils:
     def get_category(title):
         df = all_recipes[["category","title"]]
         my_category = df[df.title == title].category
+        # Return multiple categories
+        # ast.literal turns str rep of list into list
         categories = ast.literal_eval(my_category.values[0])
         return categories
 
@@ -52,26 +56,41 @@ class utils:
         my_recipe = df[df.title == title].recipe_id
         return my_recipe.values[0]
 
+    # Preprocessing (lowercasing and stop words)
     def strip_filler(str):
         stop = ["chef", "john's"]
         words = [i for i in str.split() if i.lower() not in stop]
         return " ".join(words)
 
+    # Favorite user's recipes which he has rated more than 4/5
     def known_positives(user_id,threshold=4,new_user=None):
         users = all_users[["user_id","recipe_id","rating"]]
         users = pd.concat([users,pd.DataFrame(new_user)])
 
+        # Join user dataframe and recipes dataframe
         user_preferences = pd.merge(users, recipe_lookup, on='recipe_id', how='left')
+
+        # Get user's reviews rated more than 4
         known_positives = user_preferences[(user_preferences["user_id"] == user_id)&(user_preferences["rating"] >= threshold)]
+
         known_positives_list = list(known_positives.title)
         return known_positives_list
 
+    # Create a new user from the quizz response
     def create_new_user(quiz_results):
+        # Recipes that the user choosed on the quizz
         input = [utils.title_to_id(recipe) for recipe in quiz_results]
-        new_user_id = [8888888] * len(input)
+
+        # 5555555 is a random id as the new user will be recreated each time the quizz is filled
+        new_user_id = [5555555] * len(input)
+
+        # The 5 recipes
         new_user_recipe_ids = input
+
+        # Ratings 5/5
         new_user_ratings = [5] * len(input)
 
+        # Create new ratings for this new user
         new_user = {'user_id': new_user_id,
         'recipe_id': new_user_recipe_ids,
         'rating': new_user_ratings}
@@ -92,13 +111,6 @@ class utils:
 
         return recipe_categories.drop(["calories", "ratings", "reviews", "total_mins"],axis=1)
 
-    def get_url(recipe_id):
-        try:
-            url = photo_urls.query(f'recipe_id=={recipe_id}').photo_url.values[0]
-        except: #image does not exist
-            url = "https://png.pngtree.com/element_origin_min_pic/17/08/09/28d3afc4b9471eba6f908caf6943d473.jpg"
-        return url
-
     def similar_to_cat(categories, top_N=10, all_recipes=all_recipes):
         sample_list = []
         matrix = utils.count_categories(all_recipes)
@@ -110,27 +122,18 @@ class utils:
                 pass
         return random.sample(sample_list,6)
 
+# Return matrix of ratings (row = users and col = recipes) and user_mapper, recipe_mapper, user_inv_mapper, recipe_inv_mapper
 def create_X(df):
     """
-    Generates a compressed sparse matrix "csm" dataframe.
-
     Args:
         df: pandas dataframe containing 3 columns (user_id, recipe_id, rating)
 
-    Returns:
-        X: sparse user-item matrix
-        user_mapper: dict that maps user id's to user indices
-        user_inv_mapper: dict that maps user indices to user id's
-        recipe_mapper: dict that maps recipe id's to recipe indices
-        recipe_inv_mapper: dict that maps recipe indices to movie id's
-
-    X, user_mapper, recipe_mapper, user_inv_mapper, recipe_inv_mapper = create_X(users3)
-
     """
+    # Count distict id
     M = df['user_id'].nunique()
     N = df['recipe_id'].nunique()
 
-    user_mapper = dict(zip(np.unique(df["user_id"]), list(range(M))))
+    user_mapper = dict(zip(np.unique(df["user_id"]), list(range(M)))) # {...,index : (5555555, list(0,1,2,3,4,...,M)), ...}
     recipe_mapper = dict(zip(np.unique(df["recipe_id"]), list(range(N))))
 
     user_inv_mapper = dict(zip(list(range(M)), np.unique(df["user_id"])))
@@ -140,16 +143,32 @@ def create_X(df):
     item_index = [recipe_mapper[i] for i in df['recipe_id']]
 
     X = csr_matrix((df["rating"], (user_index, item_index)), shape=(M,N))
+    # csr_matrix documentation :
+    # row = np.array([0, 0, 1, 2, 2, 2])
+    # col = np.array([0, 2, 2, 0, 1, 2])
+    # data = np.array([1, 2, 3, 4, 5, 6])
+
+    # for k in 0~5
+    # a[row_ind[k], col_ind[k]] = data[k]
+
+    #   0  1  2
+    #0 [1, 0, 2]  
+    #1 [0, 0, 3]  
+    #2 [4, 5, 6]
 
     return X, user_mapper, recipe_mapper, user_inv_mapper, recipe_inv_mapper
 
 X, user_mapper, recipe_mapper, user_inv_mapper, recipe_inv_mapper = create_X(users3)
 
+# from sklearn.metrics.pairwise import cosine_similarity
+# pickle.dump(similarity_matrix, open("similarity_matrix.pkl", "wb"))
+# similarity_matrix = cosine_similarity(X,X)
 
 class recommenders:
     def __init__(self):
         pass
 
+    # Top recipes liked by all users (random not sorted)
     def sample_popular(n=24):
         df = all_users[["rating","recipe_id"]].groupby("recipe_id").count().sort_values(by="rating",ascending=False).reset_index()
         top_1000 = [utils.recipe_id_to_title(thing) for thing in df[0:500].recipe_id]
@@ -159,16 +178,18 @@ class recommenders:
         similarity_matrix = pickle.load(open("./pickle/similarity_matrix.pkl", "rb"))
         user = user_mapper[user_id]
         # negate for most similar
-        similar_users = np.argsort(-similarity_matrix[user])[1:11] # remove original user, peak at top 10 similar users
-        sorted(-similarity_matrix[user])[1:]
+        similar_users = np.argsort(-similarity_matrix[user])[1:11] # remove original user, pick the top 10 similar users
+        #sorted(-similarity_matrix[user])[1:]
+
         recommended_recipes = []
-        # returns enough recipes ~100, so good coverage
-        # loop through all users to get top_N items, only if the recipes > threshold
+
+        # loop through all users to get top_N recipes, only if the recipes > threshold
         for i in similar_users:
             similar_user = (all_users[all_users["user_id"]==user_inv_mapper[i]])
+            # Recipes that the similar users rated more than 4/5
             recommended_recipes.extend(list(similar_user[similar_user.rating>=threshold].recipe_id))
 
-        picks = recommended_recipes[0:25]
+        picks = recommended_recipes
         # convert recipe_id to title
         picks = [recipe_lookup.query(f'recipe_id=={i}').title.values[0] for i in picks]
 
@@ -178,18 +199,25 @@ class recommenders:
         # remove duplicates & sample 6
         return sample(set(new_picks),6)
 
+    # Search for recommandations after the new user filled the quizz
     def quiz_user_user_recommender(new_user):
         pd_new_user = pd.DataFrame(new_user)
         # concat new_user rows
         new_user_df = pd.concat([users3,pd_new_user])
+
         # create a X_new
         X_new, user_mapper_new, recipe_mapper_new, user_inv_mapper_new, recipe_inv_mapper_new = create_X(new_user_df)
 
-        return recommenders.user_user_recommender(top_N=30, user_id=8888888, threshold=4, X_sparse=X_new,
+        return recommenders.user_user_recommender(top_N=30, user_id=5555555, threshold=4, X_sparse=X_new,
         user_mapper=user_mapper_new, recipe_lookup = recipe_lookup, all_users=all_users,new_user=new_user)
 
+    # recipe_categories = utils.count_categories(all_recipes).iloc[:,1:]
+    # A = csr_matrix(recipe_categories)
+    # del recipe_categories
+    # cosine_sim = cosine_similarity(A, A)
+    # pickle.dump(cosine_sim, open("cosine_sim.pkl","wb"))
 
-    def item_item_recommender(title, top_N=10, opposite=False, threshold=4, all_recipes=all_recipes, new_user=None, user_id=8888888):
+    def item_item_recommender(title, top_N=10, opposite=False, threshold=4, all_recipes=all_recipes, new_user=None, user_id=5555555):
         cosine_sim = pickle.load(open("./pickle/cosine_sim.pkl","rb"))
 
         recipe_idx = dict(zip(all_recipes['title'], list(all_recipes.index)))
@@ -209,113 +237,7 @@ class recommenders:
             sim_scores = sim_scores[1:(top_N+1)]
             similar_recipes_idx = [i[0] for i in sim_scores]
             picks = list(all_recipes['title'].iloc[similar_recipes_idx])
-            # filter out items chosen, by default filter out new user 8888888
+            # filter out items chosen, by default filter out new user 5555555
             new_picks = [pick for pick in picks if pick not in utils.known_positives(user_id,threshold,new_user)]
             # choose the top 6 from ranked new_picks to display
             return sample(new_picks[0:10],6)
-
-    def svd_recommender(user_id, new_user=None, threshold=3):
-        # for new user predictions
-        if new_user != None:
-            pd_new_user = pd.DataFrame(new_user)
-            # concat new_user rows
-            new_user_df = pd.concat([users3,pd_new_user])
-            # create a X_new
-            X_new, user_mapper_new, recipe_mapper_new, user_inv_mapper_new, recipe_inv_mapper_new = create_X(new_user_df)
-
-            X = X_new
-            user_mapper = user_mapper_new
-            recipe_inv_mapper = recipe_inv_mapper_new
-        else:
-            X, user_mapper, recipe_mapper, user_inv_mapper, recipe_inv_mapper = create_X(users3)
-            X = X
-            user_mapper = user_mapper
-            recipe_inv_mapper = recipe_inv_mapper
-
-        # calculate mean_rating_per_recipe
-        sum_ratings_per_recipe = X.sum(axis=0)
-        n_ratings_per_recipe = X.getnnz(axis=0)
-        mean_rating_per_recipe = sum_ratings_per_recipe/n_ratings_per_recipe
-        X_mean_recipe = np.tile(mean_rating_per_recipe, (X.shape[0],1))
-
-        user = user_mapper[user_id]
-        # remove item bias
-        X_norm = X - csr_matrix(X_mean_recipe)
-
-        svd = TruncatedSVD(n_components=100,random_state=42)
-        Z = svd.fit_transform(X_norm)
-        X_new = svd.inverse_transform(Z)
-        # add back the mean so it's interpretable
-        X_new = X_new + np.array(X_mean_recipe)
-
-        top_N_indices = X_new[user].argsort()[::-1]
-
-        # check that it's greater than a threshold
-        mask = X_new[user][top_N_indices]>threshold
-        num_greater_than = len(X_new[user][top_N_indices][mask])
-        # indices in order
-        top_N_indices = top_N_indices[:num_greater_than]
-        recommended = []
-        for i in top_N_indices:
-            recipe_id = recipe_inv_mapper[i]
-            recommended.append((recipe_id, utils.recipe_id_to_title(recipe_id)))
-
-        picks = [recipe[1] for recipe in recommended[0:10]]
-
-        # there may not always be more than 6 things > 4 rating
-        if len(picks) >= 6:
-            return sample(picks,6)
-        else:
-            return picks
-
-### evaluation ###
-sum_ratings_per_recipe = X.sum(axis=0)
-n_ratings_per_recipe = X.getnnz(axis=0)
-mean_rating_per_recipe = sum_ratings_per_recipe/n_ratings_per_recipe
-X_mean_recipe = np.tile(mean_rating_per_recipe, (X.shape[0],1))
-X_norm = X - csr_matrix(X_mean_recipe)
-
-X_train_norm = X_norm.todense()[0:2400]
-X_test_norm = X_norm.todense()[2400:]
-
-### Naive model ###
-naive_preds = np.tile(0,(3211,1053))
-# print(f'RMSE Train for naive model: {np.sqrt(((np.array(X_train_norm)-naive_preds[0:2400])**2).mean())}')
-# print(f'RMSE Test for naive model: {np.sqrt(((np.array(X_test_norm)-naive_preds[2400:])**2).mean())}')
-
-### Matrix Factorization Metric Calculation ###
-
-svd = TruncatedSVD(n_components=100, random_state=42)
-Z_train = svd.fit_transform(X_train_norm)
-X_train_new = svd.inverse_transform(Z_train)
-svd.explained_variance_ratio_.sum()
-
-# Sklearn Truncated SVD uses randomized_svd
-U, Sigma, VT = randomized_svd(X_norm,
-                              n_components=100,
-                              n_iter=10,
-                              random_state=None)
-
-def rank_k(k):
-    U_reduced = np.mat(U[:,:k])
-    VT_reduced = np.mat(VT[:k,:])
-    Sigma_reduced = Sigma_reduced = np.eye(k)*Sigma[:k]
-    Sigma_sqrt = np.sqrt(Sigma_reduced)
-    return U_reduced*Sigma_sqrt, Sigma_reduced, Sigma_sqrt*VT_reduced
-
-U_reduced, Sigma_reduced, VT_reduced = rank_k(30)
-
-U_new = np.dot(X_test_norm, np.dot(VT_reduced.T,np.linalg.inv(Sigma_reduced)))
-M_hat = np.dot(U_new,VT_reduced)
-
-#print(f'RMSE Train for SVD: {np.sqrt(((np.array(X_train_norm)-X_train_new)**2).mean())}')
-#print(f'RMSE Test for SVD: {np.sqrt(((np.array(X_test_norm)-np.array(M_hat))**2).mean())}')
-
-### Ranking Metrics ###
-def precision_and_recall_at_k(predictions, targets, k=6):
-    predictions = predictions[:k]
-    num_hit = len(set(predictions).intersection(set(targets)))
-    try:
-        return float(num_hit) / len(predictions), float(num_hit) / len(targets)
-    except:
-        return "Predictions must be greater than length 1"
